d2a6e11b3e5335a4b0d5d3522c87b499427748d2
Darren Chen <darrenchench@gmail.com>
2018-03-18 21:03:57 -0400

    Update retrieveplans.php

--- retrieveplans.php
+++ retrieveplans.php
@@ -3,7 +3,8 @@ define('STRIPE_SECRET_KEY','sk_test_o78wLMdkoq9mgWHnFs1B73kO');
 define('STRIPE_PUBLISHABLE_KEY','pk_test_4KUXuV4MKMN0nCyrwa2e2lQJ');
 header('Content-Type: application/json');
 $results = array();
-require 'vendor/autoload.php';
+//require 'vendor/autoload.php';
+require_once('init.php');
 \Stripe\Stripe::setApiKey(STRIPE_SECRET_KEY);
 try{
     $products  = \Stripe\Product::all();
